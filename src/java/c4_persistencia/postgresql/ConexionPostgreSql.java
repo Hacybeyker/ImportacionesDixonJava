def7eb022e0319f0d5c0d6344bb3b9d63e716183
Carlos Alfredo Osorio Perez <hacybeyker@hotmail.com>
2016-06-05 11:09:04 -0500

    Implementacion de la arquitectura N-Capas y MVC y la capa conexion

---
+++ src/java/c4_persistencia/postgresql/ConexionPostgreSql.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package c4_persistencia.postgresql;
+
+import c4_persistencia.GestorJDBC;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Carlos Alfredo Osorio Perez <hacybeyker@hotmail.com>
+ */
+public class ConexionPostgreSql extends GestorJDBC{
+
+    @Override
+    public void abrirConexion() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) { }
+        String url = "jdbc:postgresql://localhost:5432/importacionesdixon";
+        conexion = DriverManager.getConnection(url, "postgres", "123");
+    }
+}
